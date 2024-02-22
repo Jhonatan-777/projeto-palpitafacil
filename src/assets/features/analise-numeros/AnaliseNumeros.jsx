d9310189c429861484a67558756b52223180d817
JhonatanSO <jhonerex2012@gmail.com>
2024-02-22 19:13:29 -0300

    postando projeto-palpitafacil / -t

--- src/assets/features/analise-numeros/AnaliseNumeros.jsx
+++ src/assets/features/analise-numeros/AnaliseNumeros.jsx
@@ -1,62 +1,73 @@
 import { fetchData, fetchDataAnterior, fetchDataTerceiro, fetchDataQuarto } from "../../data/loreriaAPI/SorteiosLoteriaAPI"
 
-// Chama a função fetchData para obter os dados
-const {
-  numeroConcursoAtual,
-  listaDezenasAtual = [],
-} = await fetchData();
-const {
-  listaDezenasAnterior = []
-} = await fetchDataAnterior(numeroConcursoAtual);
-
-const {
-  listaDezenasTerceiro = []
-} = await fetchDataTerceiro(numeroConcursoAtual);
-const {
-  listaDezenasQuarto = []
-} = await fetchDataQuarto(numeroConcursoAtual);
-
-const allElements = listaDezenasAtual.concat(listaDezenasAnterior, listaDezenasTerceiro, listaDezenasQuarto);
-
-// Utilizando um Map para armazenar o número de repetições de cada elemento
-const repetitionCounts = new Map();
-for (const element of allElements)
+export async function processarSorteios()
 {
-  const count = repetitionCounts.get(element) || 0;
-  repetitionCounts.set(element, count + 1);
+  // Chama a função fetchData para obter os dados
+  const {
+    numeroConcursoAtual,
+    listaDezenasAtual = [],
+  } = await fetchData();
+  const {
+    listaDezenasAnterior = []
+  } = await fetchDataAnterior(numeroConcursoAtual);
+
+  const {
+    listaDezenasTerceiro = []
+  } = await fetchDataTerceiro(numeroConcursoAtual);
+  const {
+    listaDezenasQuarto = []
+  } = await fetchDataQuarto(numeroConcursoAtual);
+
+  const allElements = listaDezenasAtual.concat(listaDezenasAnterior, listaDezenasTerceiro, listaDezenasQuarto);
+
+  // Utilizando um Map para armazenar o número de repetições de cada elemento
+  const repetitionCounts = new Map();
+  for (const element of allElements)
+  {
+    const count = repetitionCounts.get(element) || 0;
+    repetitionCounts.set(element, count + 1);
+  }
+
+  // Criando um objeto aninhando os elementos repetidos com base na frequência de repetição
+  const repeatedElements = {
+    // Elementos repetidos pelo menos uma vez
+    '1': [],
+    // Elementos repetidos pelo menos duas vezes
+    '2': [],
+    // Elementos repetidos pelo menos três vezes
+    '3': [],
+    // Elementos repetidos pelo menos quatro vezes
+    '4': [],
+  };
+
+  // Percorrendo o Map e adicionando os elementos repetidos aos respectivos arrays
+  for (const [element, count] of repetitionCounts.entries())
+  {
+    if (count == 1) repeatedElements['1'].push(element);
+    if (count == 2) repeatedElements['2'].push(element);
+    if (count == 3) repeatedElements['3'].push(element);
+    if (count == 4) repeatedElements['4'].push(element);
+  }
+
+  // Ordenando os elementos do array repetidos uma vez
+  const numerosRepetidoUmaVez = repeatedElements['1'].sort((a, b) => a - b);
+  const numerosRepetidoDuasVez = repeatedElements['2'].sort((a, b) => a - b);
+  const numerosRepetidoTresVez = repeatedElements['3'].sort((a, b) => a - b);
+  const numerosRepetidoQuatroVez = repeatedElements['4'].sort((a, b) => a - b);
+
+  // Verificar quais numeros não foram sorteados
+  const allNumbers = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"];
+
+  // Encontrar os números que não estão presentes na lista de números sorteados.
+  const numbersNotDrawn = allNumbers.filter(number => !allElements.includes(number));
+
+  return {
+    numerosRepetidoUmaVez,
+    numerosRepetidoDuasVez,
+    numerosRepetidoTresVez,
+    numerosRepetidoQuatroVez,
+    numbersNotDrawn
+  };
 }
 
-// Criando um objeto aninhando os elementos repetidos com base na frequência de repetição
-const repeatedElements = {
-  // Elementos repetidos pelo menos uma vez
-  '1': [],
-  // Elementos repetidos pelo menos duas vezes
-  '2': [],
-  // Elementos repetidos pelo menos três vezes
-  '3': [],
-  // Elementos repetidos pelo menos quatro vezes
-  '4': [],
-};
-
-// Percorrendo o Map e adicionando os elementos repetidos aos respectivos arrays
-for (const [element, count] of repetitionCounts.entries())
-{
-  if (count == 1) repeatedElements['1'].push(element);
-  if (count == 2) repeatedElements['2'].push(element);
-  if (count == 3) repeatedElements['3'].push(element);
-  if (count == 4) repeatedElements['4'].push(element);
-}
-
-// Ordenando os elementos do array repetidos uma vez
-export const numerosRepetidoUmaVez = repeatedElements['1'].sort((a, b) => a - b);
-export const numerosRepetidoDuasVez = repeatedElements['2'].sort((a, b) => a - b);
-export const numerosRepetidoTresVez = repeatedElements['3'].sort((a, b) => a - b);
-export const numerosRepetidoQuatroVez = repeatedElements['4'].sort((a, b) => a - b);
-
-
-// Verificar quais numeros não foram sorteados
-const allNumbers = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"];
-
-// Encontrar os números que não estão presentes na lista de números sorteados.
-export const numbersNotDrawn = allNumbers.filter(number => !allElements.includes(number));
 
--- src/section-components/section-repetir/SectionRepetir.jsx
+++ src/section-components/section-repetir/SectionRepetir.jsx
@@ -1,12 +1,31 @@
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { numbersNotDrawn, numerosRepetidoUmaVez, numerosRepetidoDuasVez, numerosRepetidoTresVez, numerosRepetidoQuatroVez } from "../../assets/features/analise-numeros/AnaliseNumeros";
+import { processarSorteios } from "../../assets/features/analise-numeros/AnaliseNumeros";
 
 import { Section, TitleSection, Table, ContainerRepeti } from './SectionRepetir.style';
 
 const SectionRepetir = () =>
 {
-  const TabelaNumeros = ({ numeros }) => (
+  const [resultados, setResultados] = useState(null);
 
+  useEffect(() =>
+  {
+    async function fetchResultados()
+    {
+      try
+      {
+        const dados = await processarSorteios();
+        setResultados(dados);
+      } catch (error)
+      {
+        console.error("Erro ao processar os sorteios:", error);
+      }
+    }
+
+    fetchResultados();
+  }, []); // O array vazio assegura que esta função é executada apenas uma vez após a montagem do componente
+
+  const TabelaNumeros = ({ numeros }) => (
     <tbody>
       {Array.from({ length: 3 }).map((_, rowIndex) => (
         <tr key={rowIndex}>
@@ -27,48 +46,43 @@ const SectionRepetir = () =>
     <Section>
       <TitleSection>Consulte os nºs mais sorteados</TitleSection>
       <div className="container-repeti">
-        <ContainerRepeti>
-          <h3>Sorteado 4 vezes</h3>
-
-          <Table>
-            <TabelaNumeros numeros={numerosRepetidoQuatroVez} />
-          </Table>
-        </ContainerRepeti>
-
-        <ContainerRepeti>
-          <h3>Sorteado 3 vezes</h3>
-
-          <Table>
-            <TabelaNumeros numeros={numerosRepetidoTresVez} titulo="Repetiu tres vezes" />
-          </Table>
-        </ContainerRepeti>
-
-        <ContainerRepeti>
-          <h3>Sorteado 2 vezes</h3>
-
-          <Table>
-            <TabelaNumeros numeros={numerosRepetidoDuasVez} />
-          </Table>
-        </ContainerRepeti>
-
-        <ContainerRepeti>
-          <h3>Sorteado 1 vez</h3>
-
-          <Table>
-            <TabelaNumeros numeros={numerosRepetidoUmaVez} />
-          </Table>
-        </ContainerRepeti>
-
-        <ContainerRepeti>
-          <h3>Não sorteado</h3>
-
-          <Table>
-            <TabelaNumeros numeros={numbersNotDrawn} />
-          </Table>
-        </ContainerRepeti>
+        {resultados && (
+          <>
+            <ContainerRepeti>
+              <h3>Sorteado 4 vezes</h3>
+              <Table>
+                <TabelaNumeros numeros={resultados.numerosRepetidoQuatroVez} />
+              </Table>
+            </ContainerRepeti>
+            <ContainerRepeti>
+              <h3>Sorteado 3 vezes</h3>
+              <Table>
+                <TabelaNumeros numeros={resultados.numerosRepetidoTresVez} />
+              </Table>
+            </ContainerRepeti>
+            <ContainerRepeti>
+              <h3>Sorteado 2 vezes</h3>
+              <Table>
+                <TabelaNumeros numeros={resultados.numerosRepetidoDuasVez} />
+              </Table>
+            </ContainerRepeti>
+            <ContainerRepeti>
+              <h3>Sorteado 1 vez</h3>
+              <Table>
+                <TabelaNumeros numeros={resultados.numerosRepetidoUmaVez} />
+              </Table>
+            </ContainerRepeti>
+            <ContainerRepeti>
+              <h3>Não sorteado</h3>
+              <Table>
+                <TabelaNumeros numeros={resultados.numbersNotDrawn} />
+              </Table>
+            </ContainerRepeti>
+          </>
+        )}
       </div>
     </Section>
-  )
+  );
 }
 
-export default SectionRepetir
+export default SectionRepetir;
